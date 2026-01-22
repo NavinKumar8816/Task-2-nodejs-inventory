771d01b4d79e31797a86330b107e69e6ee8ee99e
Navin Kumar <Jhunu1201@gmail.com>
2026-01-22 22:45:42 +0530

    Enable CORS and finalize transactional order API

--- Backend_code/src/app.js
+++ Backend_code/src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 
+const app = express();
 const orderRoutes = require("./routes/order.routes");
+
+
+app.use(cors({
+  origin: "http://localhost:3000"
+}));
 
 app.use(express.json());
 app.use("/api", orderRoutes);
